3d12c61e57a58eff7af4a91a422bbf8a18de3531
sirx2713 <bradx2713@gmail.com>
2025-08-14 01:44:24 +0200

    Back to C++

--- main.cpp
+++ main.cpp
@@ -2,13 +2,21 @@
 
 int main() {
     double original_price;
+    double discount_rate;
     int count = 1;
 
     for (count = 1; count < 5; count++) {
         std::cout << "Enter price: ";
         std::cin >> original_price;
         std::cout << "\n";
-        const double discount_amount = original_price * 0.5;
+
+        //Enter the discount manually
+        std::cout << "Enter discount: ";
+        std::cin >> discount_rate;
+        std::cout << "\n";
+
+        //Calculations
+        double discount_amount = original_price * discount_rate;
         const double total_due = original_price - discount_amount;
 
         //Here is the bill
