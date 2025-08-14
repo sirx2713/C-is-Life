3b36b40891b1db0a5763d6f531ab5d268ab15383
sirx2713 <bradx2713@gmail.com>
2025-08-14 02:50:53 +0200

    Back to C++

--- main.cpp
+++ main.cpp
@@ -4,26 +4,57 @@ int main() {
     double original_price;
     double discount_rate;
     int count = 1;
+    std::string customer_type;
 
     for (count = 1; count < 5; count++) {
         std::cout << "Enter price: ";
         std::cin >> original_price;
         std::cout << "\n";
 
-        //Enter the discount manually
-        std::cout << "Enter discount: ";
-        std::cin >> discount_rate;
+        //Selection of the discount rate
+        std::cout << "Is customer an employee [YES or NO]:  ";
+        std::cin >> customer_type;
         std::cout << "\n";
 
-        //Calculations
-        double discount_amount = original_price * discount_rate;
-        const double total_due = original_price - discount_amount;
+        //Convert to upper case
+        for (int i = 0; i < customer_type.length(); i++) {
+            customer_type[i] = toupper(customer_type[i]);
+        }
+        if (customer_type == "YES") {
+            discount_rate = 0.25;
 
-        //Here is the bill
-        std::cout << "Client:" << count << "\n";
-        std::cout << "Original price: $" << original_price << std::endl;
-        std::cout << "Discount amount: $" << discount_amount << std::endl;
-        std::cout << "Total due: $" << total_due << std::endl;
-        std::cout << "\n";
+            //Calculations
+            double discount_amount = original_price * discount_rate;
+            const double total_due = original_price - discount_amount;
+
+            //Here is the bill
+            std::cout << "Client:" << count << "\n";
+            std::cout << "Customer:" << "Employee" << std::endl;
+            std::cout << "Original price: $" << original_price << std::endl;
+            std::cout << "Discount amount: $" << discount_amount << std::endl;
+            std::cout << "Total due: $" << total_due << std::endl;
+            std::cout << "\n";
+
+        }
+        if (customer_type == "NO") {
+            discount_rate = 0.15;
+            //Calculations
+            double discount_amount = original_price * discount_rate;
+            const double total_due = original_price - discount_amount;
+
+            //Here is the bill
+            std::cout << "Client:" << count << "\n";
+            std::cout << "Customer:" << "Regular" << std::endl;
+            std::cout << "Original price: $" << original_price << std::endl;
+            std::cout << "Discount amount: $" << discount_amount << std::endl;
+            std::cout << "Total due: $" << total_due << std::endl;
+            std::cout << "\n";
+
+        }
+        else {
+            std::cout << "Unknown Input" << std::endl;
+            std::cout << "\n";
+        }
+
     }
 }
